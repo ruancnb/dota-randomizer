b251f613298ef714c50309825fc01da2798c1c73
Ruan Barros <ruan.barros@fpf.br>
2023-05-03 17:02:26 -0400

    trying to run on cyclic

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,13 @@ import fs from "fs/promises";
 import express from "express";
 import { heroesRouter } from "./src/modules/heroesRouter.js";
 
-const server = express();
+const app = express();
 const port = process.env.PORT || 3000;
 
-server.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!, /heroes should lead you to the hero list');
 });
 
-server.use('/heroes', heroesRouter);
+app.use('/heroes', heroesRouter);
 
-server.listen(port, () => {
-    console.log('Server started on port', port);
-});
+app.listen(port);
